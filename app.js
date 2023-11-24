e52a50bfecf11d323ce756a7e1cf3f3bcdc10f0f
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2023-11-24 12:00:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 $(document).ready(function() {
-    var stackedit = new Stackedit();
+    var stackedit = new StackEdit();
 
-    // Initialize StackEdit fileChange event listener
     stackedit.on('fileChange', function(file) {
-        // Update the output area with the HTML content
         $('#output').html(file.content.html);
-        // Update the textarea with the Markdown content
+        // Update the textarea with the original Markdown content
         $('#markdown').val(file.content.text);
     });
 
     $('#markdown').on('input', function() {
         var markdownText = $(this).val();
         
-        // Open or update the file in StackEdit with the new Markdown content
         stackedit.openFile({
-            name: 'Filename', // Optional
+            name: 'Filename',
             content: {
                 text: markdownText
             }
