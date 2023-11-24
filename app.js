1665ee797318bae12df4a2743a9abb0b7a6e07a6
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2023-11-24 11:56:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
     var stackedit = new Stackedit();
 
+    // LaTeX formula you want to initialize the textarea with
+    var initialContent = '$$\\left( \\sum_{k=1}^n a_k b_k \\right)^2 \\leq \\left( \\sum_{k=1}^n a_k^2 \\right) \\left( \\sum_{k=1}^n b_k^2 \\right)$$';
+
     // Initialize StackEdit fileChange event listener
     stackedit.on('fileChange', function(file) {
         // Update the output area with the HTML content
         $('#output').html(file.content.html);
-        // Update the textarea with the Markdown content
-        $('#markdown').val(file.content.text);
     });
 
+    // Set the initial content to the textarea
+    $('#markdown').val(initialContent);
+
+    // Open StackEdit with the initial content
+    stackedit.openFile({
+        name: 'Filename', // Optional
+        content: {
+            text: initialContent
+        }
+    });
+
+    // Event handler for new input in the textarea
     $('#markdown').on('input', function() {
         var markdownText = $(this).val();
-        
+
         // Open or update the file in StackEdit with the new Markdown content
         stackedit.openFile({
             name: 'Filename', // Optional
