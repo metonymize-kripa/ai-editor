34d2c0fa0ad5797fa20aa1a403d5f2bfe1b214a3
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2023-11-24 11:38:46 -0600

    made text area update as well

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
     var stackedit = new Stackedit();
 
+    // Initialize StackEdit fileChange event listener
+    stackedit.on('fileChange', function(file) {
+        // Update the output area with the HTML content
+        $('#output').html(file.content.html);
+        // Update the textarea with the Markdown content
+        $('#markdown').val(file.content.text);
+    });
+
     $('#markdown').on('input', function() {
         var markdownText = $(this).val();
         
+        // Open or update the file in StackEdit with the new Markdown content
         stackedit.openFile({
             name: 'Filename', // Optional
             content: {
                 text: markdownText
             }
-        });
-
-        stackedit.on('fileChange', function(file) {
-            $('#output').html(file.content.html);
         });
     });
 });
