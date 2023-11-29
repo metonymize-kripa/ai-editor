39ea30c3f98ff5fa048e381ba74fa7812b39f04d
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2023-11-29 09:20:43 -0600

    Added in openai function calls

--- app.js
+++ app.js
@@ -32,4 +32,48 @@ Feel free to explore these features and more to enhance your learning and docume
             }
         });
     });
+
+    // Function to get OpenAI's response to the final transcript
+    async function getOpenAIResponse(text, instructions) {
+        const OPENAI_API_KEY = $('#api-key').val();
+        if (text.length < 5 || !OPENAI_API_KEY) {
+            return text;
+        }
+
+        const prompt = `${instructions}: ${text}`;
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                "model": "gpt-3.5-turbo",
+                messages: [{ role: "user", content: prompt }]
+            })
+        });
+
+        const data = await response.json();
+        return data.choices[0].message["content"];
+    }
+
+    // Event handler for the AI iterate button
+    $('#ai-iterate').click(async function() {
+        var text = $('#markdown').val();
+        var instructions = $('#instructions').val();
+
+        // Call OpenAI API
+        var aiResponse = await getOpenAIResponse(text, instructions);
+        
+        // Update the markdown text area with the AI's output
+        $('#markdown').val(aiResponse);
+
+        // Update the StackEdit viewer
+        stackedit.openFile({
+            name: 'Filename',
+            content: {
+                text: aiResponse
+            }
+        });
+    });
 });
